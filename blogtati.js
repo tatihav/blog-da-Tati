6b9282ad268ea5657dd8e15eaf4b75abe4ab131e
Tatiana Haveroth Barbosa <165046436+tatihav@users.noreply.github.com>
2024-07-18 15:54:26 -0300

    Add files via upload

---
+++ blogtati.js
@@ -0,0 +1,22 @@
+/*const mapaHashSection = {
+    '': document.querySelector('#inicio'),
+    '#De onde Veio': document.querySelector('#de-onde-veio'),
+    '#Trajetória': document.querySelector('#trajetoria"'),
+    '#nde foi Parar': document.querySelector('#onde-foi-parar'),
+    '#O que está Fazendo': document.querySelector('o-que-esta-fazendo'),
+
+}
+window.addEventListener('hashchange', ()=>{
+    limpaSecoes()
+    mapaHashnav[window.location.hash].style.display = 'block'
+})
+
+const secoes = document.querySelectorAll('nav')
+
+function limpasecoes(){
+    secoes.forEach(secao => secao.style.display = 'none')
+}*/
+
+
+           var nome = window.prompt(' Bem vindo ao meu Blog! qual é seu nome?')
+                window.alert('É um prazer te conhecer, '+ nome + '!')
