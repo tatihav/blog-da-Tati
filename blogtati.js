2001afc6e2908b959a0d2e751fd5710646381a04
Tatiana Haveroth Barbosa <165046436+tatihav@users.noreply.github.com>
2024-07-24 10:55:22 -0300

    Update blogtati.js

--- blogtati.js
+++ blogtati.js
@@ -1,22 +1,11 @@
-/*const mapaHashSection = {
-    '': document.querySelector('#inicio'),
-    '#De onde Veio': document.querySelector('#de-onde-veio'),
-    '#Trajetória': document.querySelector('#trajetoria"'),
-    '#nde foi Parar': document.querySelector('#onde-foi-parar'),
-    '#O que está Fazendo': document.querySelector('o-que-esta-fazendo'),
+var nome = window.prompt(' Bem vindo ao meu Blog! qual é seu nome?')
+window.alert('É um prazer te conhecer, '+ nome + '!')
 
+const btnInicio = document.getElementById('btn-inicio')
+         
+        
+
+
+btnInicio.onclick = (e) => {
+    window.scrollTo('0px','0px')
 }
-window.addEventListener('hashchange', ()=>{
-    limpaSecoes()
-    mapaHashnav[window.location.hash].style.display = 'block'
-})
-
-const secoes = document.querySelectorAll('nav')
-
-function limpasecoes(){
-    secoes.forEach(secao => secao.style.display = 'none')
-}*/
-
-
-           var nome = window.prompt(' Bem vindo ao meu Blog! qual é seu nome?')
-                window.alert('É um prazer te conhecer, '+ nome + '!')
