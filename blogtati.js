c02247fc73fe9ffe9c7fa9798587832305d7fe24
Tatiana Haveroth Barbosa <haverothtatiana@gmail.com>
2025-08-01 13:44:11 -0300

    Melhorando o desenvolvimento

--- blogtati.js
+++ blogtati.js
@@ -1,4 +1,4 @@
-var nome = window.prompt(' Bem vindo ao meu Blog! qual é seu nome?')
+/*var nome = window.prompt(' Bem vindo ao meu Blog! qual é seu nome?')
 window.alert('É um prazer te conhecer, '+ nome + '!')
 
 const btnInicio = document.getElementById('btn-inicio')
@@ -8,4 +8,4 @@ const btnInicio = document.getElementById('btn-inicio')
 
 btnInicio.onclick = (e) => {
     window.scrollTo('0px','0px')
-}
+}*/
